a368cbea341cde28eff3291155d4428c0cb4bbba
Brent Groves <brent.groves@gmail.com>
2020-06-11 11:55:01 -0400

    Added/Update source code

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 // https://github.com/vpulim/node-soap
-const mqtt = require('mqtt');
-var datetime = require('node-datetime');
+const mqtt = require("mqtt");
+var datetime = require("node-datetime");
 
 var mqttClient;
 
-var {
-  MQTT_SERVER
-} = process.env;
+var { MQTT_SERVER } = process.env;
 
 // At the bottom of the wsdl file you will find the http address of the service
 
@@ -23,7 +21,7 @@ function SoundAlarm1(transDate) {
   };
   let msgString = JSON.stringify(msg);
   console.log(msg);
-  mqttClient.publish('Alarm13319-1', msgString);
+  mqttClient.publish("Alarm13319-1", msgString);
   return;
 }
 
@@ -36,29 +34,26 @@ function SoundAlarm2(transDate) {
   };
   let msgString = JSON.stringify(msg);
   console.log(msg);
-  mqttClient.publish('Alarm13319-2', msgString);
+  mqttClient.publish("Alarm13319-2", msgString);
   return;
 }
 
 function CheckForAlarm1() {
   var dt = datetime.create();
-  var transDate = dt.format('Y-m-d H:M');
-  var min = dt.format('M');
+  var transDate = dt.format("Y-m-d H:M");
+  var min = dt.format("M");
   console.log(`CheckForAlarm1: ${min}`);
 
   if (
-    min === '00' ||
-    min === '15' ||
-    min === '30' ||
-         // (min === '54') ||
-    min === '45'
+    min === "00" ||
+    min === "15" ||
+    min === "30" ||
+    // (min === '54') ||
+    min === "45"
   ) {
-    console.log("Set Alarm2 for 2 minutes from now.")
-      let params = {
-          transDate:transDate
-      }
+    console.log("Set Alarm2 for 2 minutes from now.");
     // setInterval(SoundAlarm2.bind(params), 1000 * 60 * 2); // 2 mins past alarm1
-    setTimeout(SoundAlarm2, 1000 * 60 * 2,transDate); // 2 mins past alarm1
+    setTimeout(SoundAlarm2, 1000 * 60 * 2, transDate); // 2 mins past alarm1
     SoundAlarm1(transDate);
   }
 }
@@ -67,19 +62,19 @@ function main() {
   try {
     console.log(`Starting Alarms13319`);
     console.log(`MQTT_SERVER=${MQTT_SERVER}`);
-   // console.log(`MQTT=${MQTT}`)  // MQTT ALWAYS SAYS LOCALHOST!!
-    mqttClient  = mqtt.connect(`mqtt://${MQTT_SERVER}`)
+    // console.log(`MQTT=${MQTT}`)  // MQTT ALWAYS SAYS LOCALHOST!!
+    mqttClient = mqtt.connect(`mqtt://${MQTT_SERVER}`);
 
-mqttClient.on('connect', function () {
-  mqttClient.subscribe('presence', function (err) {
-    if (!err) {
-      mqttClient.publish('presence', 'Hello mqtt')
-    }
-  })
-})
+    mqttClient.on("connect", function () {
+      mqttClient.subscribe("presence", function (err) {
+        if (!err) {
+          mqttClient.publish("presence", "Hello mqtt");
+        }
+      });
+    });
     setInterval(CheckForAlarm1, 1000 * 60);
   } catch (err) {
-    console.log('Error !!!', err);
+    console.log("Error !!!", err);
   }
 }
 main();
